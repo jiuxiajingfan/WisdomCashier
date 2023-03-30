7cc49e3111749798564f4837b1d0a9edd760ea06
nine <jiuxiajingfan@163.com>
2023-03-30 20:53:33 +0800

    finish service client connect web and api

--- WisdomCashier-admin/src/main/java/com/li/wisdomcashier/admin/AdminApplication.java
+++ WisdomCashier-admin/src/main/java/com/li/wisdomcashier/admin/AdminApplication.java
@@ -3,7 +3,6 @@ package com.li.wisdomcashier.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.scheduling.annotation.EnableScheduling;
 import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 
 /**
@@ -16,7 +15,6 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2WebMvc;
 @SpringBootApplication(scanBasePackages = { "com.li" })
 @EnableAsync
 @EnableSwagger2WebMvc
-@EnableScheduling
 public class AdminApplication {
 
     public static void main(String[] args) {
