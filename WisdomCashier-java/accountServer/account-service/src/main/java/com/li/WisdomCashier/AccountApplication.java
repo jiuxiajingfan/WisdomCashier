8d34d96f30a7e3d4fd30124e6f2a2fa896a97f3e
nine <jiuxiajingfan@163.com>
2023-10-20 22:10:59 +0800

    swagger 聚合

--- WisdomCashier-java/accountServer/account-service/src/main/java/com/li/WisdomCashier/AccountApplication.java
+++ WisdomCashier-java/accountServer/account-service/src/main/java/com/li/WisdomCashier/AccountApplication.java
@@ -2,6 +2,7 @@ package com.li.WisdomCashier;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
--- WisdomCashier-java/common/src/main/java/com/li/WisdomCashier/exception/CommonErrorEnum.java
+++ WisdomCashier-java/common/src/main/java/com/li/WisdomCashier/exception/CommonErrorEnum.java
@@ -11,8 +11,8 @@ import lombok.Getter;
 @Getter
 public enum CommonErrorEnum implements ErrorEnum {
 
-    SYSTEM_ERROR(-1, "系统出小差了，请稍后再试哦~~"),
-    PARAM_VALID(-2, "参数校验失败{0}"),
+    SYSTEM_ERROR(500, "系统出小差了，请稍后再试哦~~"),
+    PARAM_VALID(400, "参数校验失败{0}"),
     FREQUENCY_LIMIT(-3, "请求太频繁了，请稍后再试哦~~"),
     LOCK_LIMIT(-4, "请求太频繁了，请稍后再试哦~~"),
     ;
